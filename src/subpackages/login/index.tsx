238b1ea12ddf7fc94ee71e7ebe50c5e2fc98e79d
sanwei <1304082170@qq.com>
2023-05-05 14:03:35 +0800

    update clean

--- src/subpackages/login/index.tsx
+++ src/subpackages/login/index.tsx
@@ -1,9 +1,7 @@
 import { App } from '@/components/app'
 import { Navbar } from '@/components/navbar'
-import { Sticky } from '@/components/sticky'
 
 import Bem from '@txjs/bem'
-import { useRouter } from '@tarojs/taro'
 import { defineComponent } from 'vue'
 
 import less from './index.module.less'
@@ -14,15 +12,10 @@ export default defineComponent({
   name,
 
   setup() {
-    const { params } = useRouter()
-
-    console.log(params)
-
     return () => (
       <App loading={false}>
         <Navbar />
         <App.Body>
-          <Sticky>内容</Sticky>
         </App.Body>
       </App>
     )
