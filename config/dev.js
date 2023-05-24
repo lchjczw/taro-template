193fc35e7d875b161e2ae52e165916f1571c9b45
sanwei <1304082170@qq.com>
2023-05-24 14:20:50 +0800

    update add config plugin `@tarojs/plugin-mock`

--- config/dev.js
+++ config/dev.js
@@ -2,11 +2,16 @@ const minimist = require('minimist')
 
 const plugins = []
 const ciArgs = minimist(process.argv.slice(2), {
-  boolean: 'debug'
+  boolean: 'debug',
+  boolean: 'mock'
 })
 
 if (ciArgs.debug) {
   plugins.push('@tarojs/plugin-vue-devtools')
+}
+
+if (ciArgs.mock) {
+  plugins.push('@tarojs/plugin-mock')
 }
 
 module.exports = {
